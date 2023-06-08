e6e8f3424c564f90a77dfd0eca03aa9624e3a06e
GSY <gle722@yandex.ru>
2023-06-08 19:57:57 +0300

    Решил задачу 2

--- Lesson_1/Task_2.java
+++ Lesson_1/Task_2.java
@@ -1,5 +1,30 @@
 package Lesson_1;
 
+/*
+ * 2) Вывести все простые числа от 1 до 1000
+ */
 public class Task_2 {
-    
+    public static void main(String[] args) 
+    {
+        get_prime();        
+    }
+    public static void get_prime()
+    {
+        int count = 0;
+        for (int i = 1; i <= 1000; i++) 
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                if (i % j == 0) 
+                {
+                    count += 1;                    
+                } 
+            }
+            if (count == 2) 
+            {
+                System.out.println(i); 
+            }
+            count = 0;
+        }
+    }
 }
