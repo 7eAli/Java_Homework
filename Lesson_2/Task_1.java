610090dff1ee4e23d1654cc3d88f8f8fdc072c9d
GSY <gle722@yandex.ru>
2023-06-12 12:14:08 +0300

    Решил задачу 1

--- Lesson_2/Task_1.java
+++ Lesson_2/Task_1.java
@@ -10,5 +10,26 @@ package Lesson_2;
  *    вывод: select * from students WHERE name=Ivanov AND country=Russia AND city=Moscow
  */
 public class Task_1 {
-    
+    public static void main(String[] args) {
+        StringBuilder request = new StringBuilder("select * from students WHERE");
+        String input_str = "{'name':'Ivanov', 'country':'Russia', 'city':'Moscow', 'age':'null'}";
+        input_str = input_str.replaceAll("'", "");
+        input_str = input_str.replaceAll("\\{", "");
+        input_str = input_str.replaceAll("\\}", "");
+        String[] elements = input_str.split(", ");
+        for (int i = 0; i < elements.length; i++) {
+            if (!elements[i].split(":")[1].equals("null")) {
+                request.append(" " + elements[i].split(":")[0] + "=" + elements[i].split(":")[1]);
+            }
+            if (i != elements.length - 1) 
+            {
+                if (!elements[i + 1].split(":")[1].equals("null")) 
+                {
+                    request.append(" " + "AND");
+                }
+            }
+            System.out.println(request);
+        }
+        System.out.println(request);
+    }
 }
