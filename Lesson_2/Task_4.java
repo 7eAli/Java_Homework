aa2d80f332ba6d6c9f3b2dfb086f95f33259df78
GSY <gle722@yandex.ru>
2023-06-16 15:41:27 +0300

    Решил задачу 4

--- Lesson_2/Task_4.java
+++ Lesson_2/Task_4.java
@@ -1,28 +1,36 @@
 package Lesson_2;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /*
  * 4) К калькулятору из предыдущего ДЗ добавить логирование.
  */
 public class Task_4 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException
     {
+        Logger ll = Logger.getLogger(Task_2.class.getName());
+        FileHandler fh = new FileHandler("Lesson_2\\log_task_4.txt", true);            
+        SimpleFormatter formatter = new SimpleFormatter();
+        ll.addHandler(fh);
+        fh.setFormatter(formatter);
         double a = input_number();
         String action = input_action();
         double b = input_number();
         double result = 0;
-        if (action.equals("+")) {
-            result = a + b;
-        } else if (action.equals("-")) {
-            result = a - b;
-        } else if (action.equals("*")) {
-            result = a * b;
-        } else if (action.equals("/")) {
-            result = a / b;
-        }
+        if (action.equals("+")) result = a + b;
+        else if (action.equals("-")) result = a - b;
+        else if (action.equals("*")) result = a * b;
+        else if (action.equals("/")) result = a / b;
         System.out.printf("%f %s %f = %f", a, action, b, result);
+        ll.log(Level.WARNING, "Logger on");     
+        ll.log(Level.INFO, Double.toString(a) + " " + action + " " + Double.toString(b) + " = " + result);
+        ll.log(Level.WARNING, "Logger off");     
     }
 
     public static String input_action() 
