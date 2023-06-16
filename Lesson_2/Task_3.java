d0a1bfca01eeb8e81d23bc4ad4e8bce541782ecf
GSY <gle722@yandex.ru>
2023-06-16 15:31:33 +0300

    Решил задачу 3

--- Lesson_2/Task_3.java
+++ Lesson_2/Task_3.java
@@ -1,5 +1,11 @@
 package Lesson_2;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.SortedMap;
+
+
 /*
  * 3) Дана строка (сохранить в файл и читать из файла)
  *    [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},
@@ -13,5 +19,42 @@ package Lesson_2;
  *    Студент Краснов получил 5 по предмету Физика.
  */
 public class Task_3 {
-    
+    public static void main(String[] args) throws IOException
+    {
+        StringBuilder result = new StringBuilder();
+        Charset cp = Charset.forName("UTF-8");
+        FileReader fr = new FileReader("Lesson_2\\source.txt", cp);
+        char[] info = new char[500];
+        String data = "";
+        fr.read(info);
+        for (char c : info) 
+        {            
+            data += c;
+        }
+        fr.close();        
+        data = data.replaceAll("\\[", "");
+        data = data.replaceAll(" ", "");
+        data = data.replaceAll("\\]", "");
+        data = data.replaceAll("\\{", "");
+        data = data.replaceAll("\\}", "");
+        data = data.replaceAll(",", ":");
+        data = data.replaceAll("\"", "");        
+        String[] elements = data.split(":");
+        for (int i = 1; i < elements.length; i++) 
+        {            
+            if (elements[i - 1].equals("фамилия"))
+            {
+                result.append("Студент " + elements[i] + " получил ");
+            }
+            if (elements[i - 1].equals("оценка"))
+            {
+                result.append(elements[i] + " по предмету ");
+            }
+            if (elements[i - 1].equals("предмет"))
+            {
+                result.append(elements[i] + ".\n");
+            }
+        }
+        System.out.println(result);           
+    }
 }
